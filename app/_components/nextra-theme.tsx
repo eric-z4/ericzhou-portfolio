63efba52167edcf06201ae1b6916283e3e642b44
Eric Zhou <ericzhougit@gmail.com>
2026-01-05 10:18:58 -1000

    Removed some template stuff

--- app/_components/nextra-theme.tsx
+++ app/_components/nextra-theme.tsx
@@ -1,5 +1,4 @@
 import type { PageMapItem } from 'nextra';
-import { version } from 'nextra/package.json';
 import type { FC, ReactNode } from 'react';
 import { Footer } from './footer';
 import { Navbar } from './navbar';
@@ -11,16 +10,6 @@ export const NextraTheme: FC<{
 }> = ({ children, pageMap }) => {
     return (
         <>
-            <h1
-                style={{
-                    margin: 0,
-                    padding: 20,
-                    background: 'lightslategray',
-                    fontWeight: 'normal'
-                }}
-            >
-                Custom theme demo for <strong>Nextra {version}</strong>
-            </h1>
             <Navbar pageMap={pageMap} />
             <div style={{ display: 'flex' }}>
                 <Sidebar pageMap={pageMap} />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 ﻿import type { Metadata } from 'next';
-import { Head } from 'nextra/components';
 import { getPageMap } from 'nextra/page-map';
 import type { FC, ReactNode } from 'react';
 import { NextraTheme } from './_components/nextra-theme';
@@ -15,7 +14,6 @@ const RootLayout: FC<{ children: ReactNode; }> = async ({ children }) => {
     const pageMap = await getPageMap();
     return (
         <html lang="en" dir="ltr">
-            <Head faviconGlyph="✦" />
             <body style={{ margin: 0 }}>
                 <NextraTheme pageMap={pageMap}>{children}</NextraTheme>
             </body>
