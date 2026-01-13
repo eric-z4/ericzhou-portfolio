52103dddef6a71b23719496092f64c4ae431c9b9
Eric Zhou <ericzhougit@gmail.com>
2026-01-12 20:46:33 -1000

    Projects squared

--- app/_projects/project_wrapper.tsx
+++ app/_projects/project_wrapper.tsx
@@ -4,12 +4,13 @@ import ProjectTemplate from './project_template.mdx';
 
 const ProjectWrapper: FC = () => {
     return (
-        <div className='w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-auto py-5 gap-5'>
+        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr py-5 gap-5'>
             {projects.map((item, i) => {
                 return (
                     <ProjectTemplate
                         key={i + item.image}
                         image={'/images/' + item.image}
+                        title={item.title}
                     />);
             })}
         </div>
