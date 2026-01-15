cb8032e77117c9cd98814ce6829d412615456a46
Eric Zhou <ericzhougit@gmail.com>
2026-01-15 09:44:35 -1000

    Update project_wrapper.tsx

--- app/_projects/project_wrapper.tsx
+++ app/_projects/project_wrapper.tsx
@@ -6,27 +6,13 @@ import Image from 'next/image';
 
 const ProjectWrapper: FC = () => {
     const [lastClickedDiv, setLastClickedDiv] = useState("");
-    const [isClickOnce, setIsClickOnce] = useState(false);
 
     function handleProjectClick(text: string, index: number) {
-        if (typeof window !== undefined) {
-            if (matchMedia('(pointer: coarse)').matches) {
-                if (lastClickedDiv === (text + index)) {
-                    if (!isClickOnce) {
-                        setIsClickOnce(true);
-                    } else {
-                        alert(text);
-                    }
-                } else {
-                    setIsClickOnce(false);
-                }
-                setLastClickedDiv(text + index);
-            } else {
-                setLastClickedDiv("");
-                setIsClickOnce(false);
-                alert(text);
-            }
+        console.log(lastClickedDiv + " / " + (text + index))
+        if (lastClickedDiv === (text + index)) {
+            alert(text);
         }
+        setLastClickedDiv(text + index);
     }
 
     return (
@@ -36,8 +22,8 @@ const ProjectWrapper: FC = () => {
                     <div
                         id={item.title + i}
                         key={item.title + i}
-                        tabIndex={(typeof window !== undefined) ? (matchMedia('(pointer: coarse)').matches ? 0 : -1) : -1}
-                        className = 'relative group aspect-square w-full overflow-hidden mx-auto outline-3 outline-transparent hover:outline-(--project-outline) pointer-coarse:focus:outline-(--project-outline) transition-[outline-color] delay-0 duration-300 ease-in-out' 
+                        tabIndex={0}
+                        className = 'relative group aspect-square w-full overflow-hidden mx-auto outline-3 outline-transparent hover:outline-(--project-outline) focus:outline-(--project-outline) transition-[outline-color] delay-0 duration-300 ease-in-out' 
                         onClick={() => handleProjectClick(item.title, i)}
                     >
                         <Image
@@ -47,17 +33,20 @@ const ProjectWrapper: FC = () => {
                             className='object-cover'
                             loading="eager"
                         />
-                        <div className='absolute inset-x-0 bg-linear-to-b from-(--project-fade) from-10% group-hover:from-90% pointer-coarse:group-focus:from-90% to-transparent to-70% group-hover:to-100% pointer-coarse:group-focus:to-100% opacity-85 h-full p-2 text-lg text-(--project-text) select-none transition-[--tw-gradient-from-position] delay-0 duration-200 ease-in-out'>
+                        <div className='absolute inset-x-0 bg-linear-to-b from-(--project-fade) from-10% group-hover:from-90% group-focus:from-90% to-transparent to-70% group-hover:to-100% group-focus:to-100% opacity-85 h-full p-2 text-lg text-(--project-text) select-none transition-[--tw-gradient-from-position] delay-0 duration-200 ease-in-out'>
                             <h4 className='opacity-100 wrap-anywhere'>
                                 {item.title}
                             </h4>
-                            <hr className='my-2 opacity-0 group-hover:opacity-100 pointer-coarse:group-focus:opacity-100 transition-opacity delay-0 duration-200 ease-in-out'></hr>
-                            <p className='opacity-0 group-hover:opacity-100 pointer-coarse:group-focus:opacity-100 text-sm transition-opacity delay-0 duration-200 ease-in-out'>
+                            <hr className='my-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity delay-0 duration-200 ease-in-out'></hr>
+                            <p className='opacity-0 group-hover:opacity-100 group-focus:opacity-100 text-sm transition-opacity delay-0 duration-200 ease-in-out'>
                                 {item.date}
                             </p>
-                            <p className='opacity-0 group-hover:opacity-100 pointer-coarse:group-focus:opacity-100 wrap-break-word pt-4 text-sm transition-opacity delay-0 duration-200 ease-in-out'>
+                            <p className='opacity-0 group-hover:opacity-100 group-focus:opacity-100 wrap-break-word pt-4 text-sm transition-opacity delay-0 duration-200 ease-in-out'>
                                 {item.description}
                             </p>
+                        </div>
+                        <div className='absolute w-full text-center bottom-1/8'>
+                            <p className='opacity-0 group-hover:opacity-100 group-focus:opacity-100 text-sm transition-opacity delay-0 duration-200 ease-in-out'>Click/tap again for more details</p>
                         </div>
                     </div>
                 );
