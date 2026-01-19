cdae3fbdc6c73e5e4cfc783dd53543551708088f
Eric Zhou <ericzhougit@gmail.com>
2026-01-18 20:04:22 -1000

    Fixed sidepanel being off

--- app/_components/sidepanel.tsx
+++ app/_components/sidepanel.tsx
@@ -5,7 +5,7 @@ import { themes } from '../_themes/themes.json';
 
 export const SidepanelThemeButton: FC<{ bg: string, themeName: string, tooltip: string, toggleTooltip: string; }> = ({ bg, themeName, tooltip, toggleTooltip }) => {
     const circularDiv: string = "flex aspect-square rounded-full relative";
-    const circularInput: string = "-m-1 aspect-square rounded-full appearance-none cursor-pointer";
+    const circularInput: string = "w-full m-0 aspect-square rounded-full appearance-none cursor-pointer";
     const borderDiv: string = "border-2 border-black";
     const borderInput: string = "opacity-0 checked:opacity-100 hover:not-checked:opacity-100 border-2 border-(--foreground-primary) checked:border-y-(--foreground-primary) checked:border-x-transparent transition-opacity duration-400";
     const tooltipClass: string = "absolute w-max p-1 top-1/2 left-[125%] -translate-y-1/2 bg-black rounded-sm text-sm select-none";
@@ -44,10 +44,10 @@ export const SidepanelThemeButton: FC<{ bg: string, themeName: string, tooltip: 
 
 export const Sidepanel: FC = () => {
     const [tooltipToggle, setTooltipToggle] = useState("");
-    const [leftVal, setLeftVal] = useState(-16);
+    const [leftVal, setLeftVal] = useState(-14);
 
     function handleTabClick() {
-        setLeftVal(leftVal ^ -16);
+        setLeftVal(leftVal ^ -14);
         if (tooltipToggle === "") {
             setTooltipToggle("peer-checked:opacity-80");
         } else {
@@ -56,7 +56,7 @@ export const Sidepanel: FC = () => {
     }
 
     return (
-        <div className='fixed z-1 top-1/6 w-14 md:w-16 h-2/3 transition-[left] duration-200 ease-in-out' style={{ left: `calc(var(--spacing) * ${leftVal})` }}>
+        <div className='fixed z-1 top-1/6 w-14 h-2/3 transition-[left] duration-200 ease-in-out' style={{ left: `calc(var(--spacing) * ${leftVal})` }}>
             <div
                 className='absolute block rounded-r-lg bottom-4 -right-5 w-5 h-22 bg-(--sidepanel-bg) text-center select-none'
                 onClick={handleTabClick}
@@ -64,8 +64,8 @@ export const Sidepanel: FC = () => {
                 &gt;
             </div>
             <div className='relative h-full'>
-                <div className='absolute bottom-0 flex flex-col-reverse w-14 md:w-16 rounded-r-lg gap-3 p-2 bg-(--sidepanel-bg)'>
-                    <p className='text-sm'>Themes</p>
+                <div className='absolute bottom-0 flex flex-col-reverse w-14 rounded-r-lg gap-3 p-2 bg-(--sidepanel-bg)'>
+                    <p className='text-xs'>Themes</p>
                     {themes.map((theme, i) => {
                         return <SidepanelThemeButton
                             key={theme.name + i}
