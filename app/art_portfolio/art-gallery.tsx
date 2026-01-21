91cb6baeacc113b6055a80d6b57a578314d05d9d
Eric Zhou <ericzhougit@gmail.com>
2026-01-20 20:59:40 -1000

    Added embed videos

--- app/art_portfolio/art-gallery.tsx
+++ app/art_portfolio/art-gallery.tsx
@@ -1,10 +1,27 @@
 'use client';
 
 import { useState, type FC } from 'react';
+import { BsPlusLg } from "react-icons/bs";
 import Image from 'next/image';
 import { artworks } from './arts.json';
 
-const CarouselItem: FC<{ imageRoute: string, imageAlt: string, imageWidth: number, imageHeight: number; }> = ({ imageRoute, imageAlt, imageWidth, imageHeight }) => {
+const CarouselVideo: FC<{ videoRoute: string, videoName: string, videoWidth: number, videoHeight: number; }> = ({ videoRoute, videoName, videoWidth, videoHeight }) => {
+    return (
+        <div className='flex basis-32 grow-1 h-full'>
+            <iframe
+                width={videoWidth}
+                height={videoHeight}
+                src={`${videoRoute};controls=0`}
+                title={videoName}
+                className='w-full h-full object-contain bg-zinc-800'
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            />
+        </div>
+    );
+}
+
+const CarouselImage: FC<{ imageName: string, imageRoute: string, imageAlt: string, imageWidth: number, imageHeight: number; }> = ({ imageName, imageRoute, imageAlt, imageWidth, imageHeight }) => {
     return (
         <div className='flex basis-32 grow-1 h-full'>
             <Image
@@ -41,22 +58,31 @@ const ArtGallery: FC = () => {
                         <div className='w-full grow-8 flex basis-32 bg-blue-100 aspect-16/9 overflow-x-auto'>
                             <div className='flex shrink-0 h-full' style={{ flexBasis: `${Math.max(100 * artworks[cardImageIndex].carouselContent.length, 100)}%` }}>
                                 {artworks[cardImageIndex].carouselContent.length === 0 ?
-                                    <CarouselItem
+                                    <CarouselImage
+                                        imageName={artworks[cardImageIndex].name}
                                         imageRoute={artworks[cardImageIndex].thumbnail}
                                         imageAlt={artworks[cardImageIndex].name}
                                         imageWidth={artworks[cardImageIndex].thumbnailWidth}
                                         imageHeight={artworks[cardImageIndex].thumbnailHeight}
                                     /> :
                                     artworks[cardImageIndex].carouselContent.map((item, i) => {
-                                    return (
-                                        <CarouselItem
-                                            key={item.name + i}
-                                            imageRoute={item.image}
-                                            imageAlt={item.name}
-                                            imageWidth={item.width}
-                                            imageHeight={item.height}
-                                        />
-                                    );
+                                        return (item.type === "image" ? 
+                                            <CarouselImage
+                                                key={item.name + i}
+                                                imageName={item.name}
+                                                imageRoute={item.route}
+                                                imageAlt={item.name}
+                                                imageWidth={item.width}
+                                                imageHeight={item.height}
+                                            /> :
+                                            <CarouselVideo
+                                                key={item.name + i}
+                                                videoName={item.name}
+                                                videoRoute={item.route}
+                                                videoWidth={item.width}
+                                                videoHeight={item.height}
+                                            />
+                                        );
                                 })}
                             </div>
                         </div>
@@ -79,7 +105,9 @@ const ArtGallery: FC = () => {
                             loading="eager"
                         />
                         {item.carouselContent.length > 0 ?
-                            <div className='absolute bg-black/40 size-8 right-2 bottom-2 rounded-full'></div> :
+                            <div className='absolute bg-black/40 size-8 right-2 bottom-2 rounded-full'>
+                                <BsPlusLg className='mx-auto h-full' />
+                            </div> :
                             <></>
                         }
                     </div>
