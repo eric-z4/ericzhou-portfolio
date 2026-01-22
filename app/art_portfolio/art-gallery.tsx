1c3509a8be37065c9e6cadac81d59d60f6907649
Eric Zhou <ericzhougit@gmail.com>
2026-01-21 20:54:25 -1000

    Implemented navigation bttns for carousel

--- app/art_portfolio/art-gallery.tsx
+++ app/art_portfolio/art-gallery.tsx
@@ -5,15 +5,15 @@ import { BsPlusLg } from "react-icons/bs";
 import Image from 'next/image';
 import { artworks } from './arts.json';
 
-const CarouselVideo: FC<{ videoRoute: string, videoName: string, videoWidth: number, videoHeight: number; }> = ({ videoRoute, videoName, videoWidth, videoHeight }) => {
+const CarouselVideo: FC<{ id: string, videoRoute: string, videoName: string, videoWidth: number, videoHeight: number; }> = ({ id, videoRoute, videoName, videoWidth, videoHeight }) => {
     return (
-        <div className='flex basis-32 grow-1 h-full'>
+        <div id={id} className='flex flex-none basis-full grow-1 h-full snap-always snap-center'>
             <iframe
                 width={videoWidth}
                 height={videoHeight}
                 src={`${videoRoute};controls=0`}
                 title={videoName}
-                className='w-full h-full object-contain bg-zinc-800'
+                className='w-full h-full object-contain bg-transparent'
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
             />
@@ -21,16 +21,16 @@ const CarouselVideo: FC<{ videoRoute: string, videoName: string, videoWidth: num
     );
 }
 
-const CarouselImage: FC<{ imageName: string, imageRoute: string, imageAlt: string, imageWidth: number, imageHeight: number; }> = ({ imageName, imageRoute, imageAlt, imageWidth, imageHeight }) => {
+const CarouselImage: FC<{ id: string, imageRoute: string, imageAlt: string, imageWidth: number, imageHeight: number; }> = ({ id, imageRoute, imageAlt, imageWidth, imageHeight }) => {
     return (
-        <div className='flex basis-32 grow-1 h-full'>
+        <div id={id} className='flex flex-none basis-full grow-1 h-full'>
             <Image
                 src={"/images/art_portfolio/" + imageRoute}
                 alt={imageAlt}
                 width={imageWidth}
                 height={imageHeight}
                 loading="eager"
-                className='w-full h-full object-contain bg-zinc-800'
+                className='w-full h-full object-contain bg-transparent snap-always snap-center'
             />
         </div>
     );
@@ -38,6 +38,7 @@ const CarouselImage: FC<{ imageName: string, imageRoute: string, imageAlt: strin
 
 const ArtGallery: FC = () => {
     const [cardImageIndex, setCardImageIndex] = useState(0);
+    const [carouselIndex, setCarouselIndex] = useState(0);
     const [cardVisibility, setCardVisibility] = useState(false);
 
     function handleArtClick(index : number) {
@@ -45,54 +46,70 @@ const ArtGallery: FC = () => {
         setCardImageIndex(index < 0 ? cardImageIndex : index);
     }
 
+    function handleScrollClick(contentNames: string[], index: number) {
+        if (contentNames.length > 0 && index >= 0 && index < contentNames.length) {
+            document.getElementById(contentNames[index] + index)?.scrollIntoView();
+            setCarouselIndex(index);
+        }
+    }
+
     return (
         <div className='columns-1 md:columns-2 xl:columns-3 gap-3'>
             <div
                 className='z-1 fixed flex flex-col w-full h-full left-0 top-0 bg-black/30 backdrop-blur-xs transition-opacity duration-300 ease-in-out select-none'
                 style={{ opacity: cardVisibility ? 1 : 0, pointerEvents: cardVisibility ? 'auto' : 'none' }}
-                onClick={() => handleArtClick(-1)}
             >
-                <div className='flex grow-1 bg-transparent basis-8'></div>
+                <div className='flex grow-1 bg-transparent basis-8' onClick={() => handleArtClick(-1)}></div>
                 <div className='flex grow-10 bg-transparent basis-16'>
-                    <div className='w-full md:w-4/5 md:mx-auto flex flex-col bg-(--midground) p-4 rounded-md'>
-                        <div className='w-full grow-8 flex basis-32 bg-blue-100 aspect-16/9 overflow-x-auto'>
-                            <div className='flex shrink-0 h-full' style={{ flexBasis: `${Math.max(100 * artworks[cardImageIndex].carouselContent.length, 100)}%` }}>
-                                {artworks[cardImageIndex].carouselContent.length === 0 ?
-                                    <CarouselImage
-                                        imageName={artworks[cardImageIndex].name}
-                                        imageRoute={artworks[cardImageIndex].thumbnail}
-                                        imageAlt={artworks[cardImageIndex].name}
-                                        imageWidth={artworks[cardImageIndex].thumbnailWidth}
-                                        imageHeight={artworks[cardImageIndex].thumbnailHeight}
-                                    /> :
-                                    artworks[cardImageIndex].carouselContent.map((item, i) => {
-                                        return (item.type === "image" ? 
-                                            <CarouselImage
-                                                key={item.name + i}
-                                                imageName={item.name}
-                                                imageRoute={item.route}
-                                                imageAlt={item.name}
-                                                imageWidth={item.width}
-                                                imageHeight={item.height}
-                                            /> :
-                                            <CarouselVideo
-                                                key={item.name + i}
-                                                videoName={item.name}
-                                                videoRoute={item.route}
-                                                videoWidth={item.width}
-                                                videoHeight={item.height}
-                                            />
-                                        );
-                                })}
-                            </div>
+                    <div className='flex grow-1 bg-transparent basis-0 md:basis-8' onClick={() => handleArtClick(-1)}></div>
+                    <div className='flex flex-col grow-1 basis-full md:basis-4/5 bg-(--midground) p-4 rounded-md'>
+                        <div
+                            className='w-full grow-8 flex basis-32 bg-zinc-800 aspect-16/9 overflow-x-scroll snap-x snap-mandatory scroll-smooth'
+                            style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
+                        >
+                            {artworks[cardImageIndex].carouselContent.length === 0 ?
+                                <CarouselImage
+                                    id={artworks[cardImageIndex].name}
+                                    imageRoute={artworks[cardImageIndex].thumbnail}
+                                    imageAlt={artworks[cardImageIndex].name}
+                                    imageWidth={artworks[cardImageIndex].thumbnailWidth}
+                                    imageHeight={artworks[cardImageIndex].thumbnailHeight}
+                                /> :
+                                artworks[cardImageIndex].carouselContent.map((item, i) => {
+                                    return (item.type === "image" ? 
+                                        <CarouselImage
+                                            key={item.name + i}
+                                            id={item.name + i}
+                                            imageRoute={item.route}
+                                            imageAlt={item.name}
+                                            imageWidth={item.width}
+                                            imageHeight={item.height}
+                                        /> :
+                                        <CarouselVideo
+                                            key={item.name + i}
+                                            id={item.name + i}
+                                            videoName={item.name}
+                                            videoRoute={item.route}
+                                            videoWidth={item.width}
+                                            videoHeight={item.height}
+                                        />
+                                    );
+                            })}
                         </div>
                         <div className='w-full shrink-0 basis-fit bg-transparent'>
                             <h2>{artworks[cardImageIndex].name}</h2>
                             <p>{artworks[cardImageIndex].description}</p>
+                            <div className='bg-red-800' onClick={() => handleScrollClick(artworks[cardImageIndex].carouselContent.map(item => item.name), carouselIndex - 1)}>
+                                &lt;
+                            </div>
+                            <div className='bg-blue-800' onClick={() => handleScrollClick(artworks[cardImageIndex].carouselContent.map(item => item.name), carouselIndex + 1)}>
+                                &gt;
+                            </div>
                         </div>
                     </div>
+                    <div className='flex grow-1 bg-transparent basis-0 md:basis-8' onClick={() => handleArtClick(-1)}></div>
                 </div>
-                <div className='flex grow-1 bg-transparent basis-8'></div>
+                <div className='flex grow-1 bg-transparent basis-8' onClick={() => handleArtClick(-1)}></div>
             </div>
             {artworks.map((item, i) => {
                 return (
