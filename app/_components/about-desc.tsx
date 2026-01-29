a2974c1340cb5c5f309bcdcff0fdf0b93c838478
Eric Zhou <ericzhougit@gmail.com>
2026-01-29 09:43:11 -1000

    Filled website about desc

---
+++ app/_components/about-desc.tsx
@@ -0,0 +1,13 @@
+import { type FC } from 'react';
+
+const AboutDesc: FC = () => {
+    return (
+        <div className='text-center'>
+            <h4>{'Welcome to my website! My name is Eric, and I enjoy developing and designing a variety of creative media. I studied Information & Computer Science (B.A.) and Creative Compuatational Media (Certificate) at the Unviersity of Hawai\'i at Manoa.'}</h4>
+            <br />
+            <h4>Check out some of the things I made below!</h4>
+        </div>
+    );
+};
+
+export default AboutDesc;
